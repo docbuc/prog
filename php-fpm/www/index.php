cd05fcb06690e37038d6e17fb9319b940a25fabb
Bernd Oeggl <bernd@webman.at>
2021-07-01 12:34:25 +0200

    update ruby and fpm prog examples

--- php-fpm/www/index.php
+++ php-fpm/www/index.php
@@ -2,18 +2,17 @@
 // Datei: prog/php-fpm/www/index.php
 require 'vendor/autoload.php';
 $Parsedown = new Parsedown();
-$img = 'port_de_la_capte.jpg';
+$img = 'erto.jpg';
 $exif = exif_read_data($img);
 $md =  <<<EOD
 ![pic]($img)
 
 # Exif Info:
 
-* Model: {$exif['Model']}
+* Kamera: {$exif['Make']}/{$exif['Model']}
 * Aufnahmedatum: {$exif['DateTimeOriginal']}
 * Belichtungszeit: {$exif['ExposureTime']}
 * Blende: {$exif['FNumber']}
 * ISO: {$exif['ISOSpeedRatings']}
 EOD;
 echo $Parsedown->text($md);
-
